edaa2d7f1173604310d2d344d8f1fb41dc079641
Shobhit Kumar <kumar@shobhit.info>
2018-10-20 16:42:59 +0530

    Add a module parameter to sample module
    Signed-off-by: Shobhit Kumar <kumar@shobhit.info>

--- hello.c
+++ hello.c
@@ -2,15 +2,18 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
+static char *who = "World";
+module_param(who, charp, S_IRUGO|S_IWUSR);
+
 static int __init hello_init(void)
 {
-	printk("Hello World\n");
+	printk("Hello %s\n", who);
 	return 0;
 }
 
 static void __exit hello_exit(void)
 {
-	printk("Bye bye world\n");
+	printk("Bye bye %s\n", who);
 }
 
 module_init(hello_init);
