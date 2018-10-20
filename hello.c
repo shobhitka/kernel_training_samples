9a596922a97bfa0fdb1f76ff7397b7553f44385e
Shobhit Kumar <kumar@shobhit.info>
2018-10-20 17:02:37 +0530

    Add support to print module live time in seconds
    Signed-off-by: Shobhit Kumar <kumar@shobhit.info>

--- hello.c
+++ hello.c
@@ -1,19 +1,24 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/time.h>
 
 static char *who = "World";
 module_param(who, charp, S_IRUGO|S_IWUSR);
 
+struct timeval tv1, tv2;
+
 static int __init hello_init(void)
 {
+	do_gettimeofday(&tv1);
 	printk("Hello %s\n", who);
 	return 0;
 }
 
 static void __exit hello_exit(void)
 {
-	printk("Bye bye %s\n", who);
+	do_gettimeofday(&tv2);
+	printk("Bye bye %s. Time loaded = %ld seconds\n", who, tv2.tv_sec - tv1.tv_sec);
 }
 
 module_init(hello_init);
