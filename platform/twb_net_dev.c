f11324cb4d54c43d2c47a877736dff0c5e70ef7c
Shobhit Kumar <kumar@shobhit.info>
2018-11-10 17:48:22 +0530

    Add a moduel to insert a platform device
    Signed-off-by: Shobhit Kumar <kumar@shobhit.info>

---
+++ platform/twb_net_dev.c
@@ -0,0 +1,44 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/slab.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/mutex.h>
+#include <linux/fs.h>
+#include <linux/errno.h>
+#include <linux/uaccess.h>
+#include <linux/wait.h>
+#include <linux/platform_device.h>
+
+static struct resource twbnet_dev_resources[] = {
+};
+
+static struct platform_device twbnet_dev = {
+	.name = "twbnet",
+	.id = 1,
+	.num_resources = ARRAY_SIZE(twbnet_dev_resources),
+	.resource = twbnet_dev_resources,
+};
+
+int twbnet_dev_init(void)
+{
+	printk ("[twbnet]: Adding new platform device\n");
+
+	platform_device_register(&twbnet_dev);
+	return 0;
+}
+
+void twbnet_dev_cleanup (void)
+{
+	printk ("[twbnet]: Releasing device\n");
+	platform_device_unregister(&twbnet_dev);
+	return;
+}
+
+module_init (twbnet_dev_init);
+module_exit (twbnet_dev_cleanup);
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Pseudo Network Device");
+MODULE_AUTHOR("Shobhit Kumar");
